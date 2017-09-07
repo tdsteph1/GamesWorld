75de66f40bf12bbb8ff27117ffc63d592aea5056
HelloBetafish <bethany.pfeister@gmail.com>
2017-09-06 20:19:41 -0700

    changed planets from hover to click to grow

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
+
+  $("span").hide();
+
+  $("img").on("click",function() {
+    var size = $(this).attr("size");
+    if (size ==="small"){
+      $(this).animate({ height:"250px", width: "250px"});
+      $(this).attr("size", "large");
+      $(this).css("border","5px solid white");
+      $(this).next().show(500);}
+      
+    else{
+      $(this).animate({ height:"150px", width: "150px"});
+      $(this).attr("size", "small");
+      $(this).css("border","none");
+      $(this).next().hide();}
+  });
   
   // $("a").on("mouseover", function() {
   // 	$(this).children("img").animate({ height: "250px", width: "250px" });
+  //   $(this).children("span").show();
   // });
 
   // $("a").on("mouseout", function() {
   // 	$(this).children("img").animate({ height: "150px", width: "150px"});
+  //   $(this).children("span").hide();
   // });
   
 // Alternate code for above
